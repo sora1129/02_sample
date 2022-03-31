74f0f8753670d947c655d12c4fcca81dde153882
sora1129 <64789798+sora1129@users.noreply.github.com>
2022-03-31 16:21:16 +0900

    npm i gulp sass

--- gulpfile.js
+++ gulpfile.js
@@ -1,15 +1,29 @@
-var gulp = require('gulp');
-var sass = require('gulp-sass');
+const { src, dest, watch, series } = require('gulp');
+// var gulp = require('gulp');
+// var sass = require('gulp-sass');
+const sass = require('gulp-sass')(require('sass'));
 
-gulp.task('sass', function() {
-  return gulp.src('./sass/**/*.scss')
-    .pipe(sass({outputStyle: 'expanded'}))
-    .pipe(gulp.dest('./css'));
-});
+// gulp.task('sass', function() {
+//   return gulp.src('./sass/**/*.scss')
+//     .pipe(sass({outputStyle: 'expanded'}))
+//     .pipe(gulp.dest('./css'));
+// });
+const cssSass = () => {
+	return src('./sass/**/*.scss')
+	.pipe(sass({outputStyle: 'expanded'}))
+	.pipe(dest('./css'));
+}
 
-gulp.task('sass:watch', function() {
-  gulp.watch('./sass/**/*.scss', gulp.task('sass'));
-});
+exports.sass = series(cssSass);
+
+// gulp.task('sass:watch', function() {
+//   gulp.watch('./sass/**/*.scss', gulp.task('sass'));
+// });
+// watch(globs, [options], [task])
+
+exports.watch = () => {
+	watch('./sass/**/*.scss', series(cssSass));
+};
 
 // 初版・2刷の方へ
 // gulp v4 から上記の書き方に変更されています
